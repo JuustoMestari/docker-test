74917349009aee34c40c0c59955e038333dbc57e
David Davenne <david@davenne.be>
2014-07-08 19:02:27 +0300

    update

--- index.php
+++ index.php
@@ -2,4 +2,4 @@
 
 <h2>My content has been pulled from <a href="https://github.com/JuustoMestari/docker-test"> this repo </a> !</h2>
 
-<h3>Today is <?php echo date("d.m.y")." and the time is ".date("H:i:s")?>
+<h3>Today is <?php echo date("d.m.y")." and the time is ".date("H:i:s")." (Server Time)"?>
