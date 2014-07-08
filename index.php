120c9e28108af2ec17f6977606e9172f83588604
David Davenne <david@davenne.be>
2014-07-08 17:54:20 +0300

    update

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <h1>It works !</h1>
 
-<h2>My content has been pulled from : <a href="https://github.com/JuustoMestari/docker-test"> this repo </a> !</h2>
+<h2>My content has been pulled from <a href="https://github.com/JuustoMestari/docker-test"> this repo </a> !</h2>
 
 <?php
  phpinfo();
